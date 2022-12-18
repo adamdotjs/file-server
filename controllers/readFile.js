993c52089c8ba43051cad05fa19ca6133298700f
Adam Shea <avidworks@live.com>
2022-12-18 15:40:24 -0500

    convert to async and add image helper

--- controllers/readFile.js
+++ controllers/readFile.js
@@ -1,38 +1,41 @@
 import soap from "soap";
+import { readImage } from "./readImage.js";
 
 const url = `https://internet:47tig7D@epace.tigerpress.com/rpc/services/ReadObject?wsdl`;
 
-const readFiles = async (req, res) => {
+// const readFile = (req, res) => {
+//   const args = {
+//     inventoryItem: {
+//       id: req.params.id,
+//     },
+//   };
+
+//   soap.createClient(url, {}, function (err, client) {
+//     if (client) {
+//       client.setSecurity(new soap.BasicAuthSecurity("internet", "47tig7D"));
+//       client.readInventoryItem(args, (err, result) => {
+//         const image = readImage(result.out.thumbnail);
+//         console.log(image);
+//         res.render("file.ejs", { file: result.out, image });
+//       });
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// };
+
+const readFile = async (req, res) => {
   const args = {
-    name: "readInventoryItem",
-    id: req.params.id,
+    inventoryItem: { id: req.params.id },
   };
-  try {
-    const client = await soap.createClientAsync(url);
-    const response = await client(args);
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(400).send("An error occured");
-  }
+
+  const client = await soap.createClientAsync(url);
+  client.setSecurity(new soap.BasicAuthSecurity("internet", "47tig7D"));
+
+  const response = await client.readInventoryItemAsync(args);
+  const image = await readImage(response[0].out.thumbnail);
+
+  res.render("file.ejs", { file: response[0].out, image });
 };
 
-const readFile = (req, res) => {
-  const args = {
-    inventoryItem: {
-      id: req.params.id,
-    },
-  };
-
-  soap.createClient(url, {}, function (err, client) {
-    if (client) {
-      client.setSecurity(new soap.BasicAuthSecurity("internet", "47tig7D"));
-      client.readInventoryItem(args, function (err, result) {
-        res.render("file.ejs", { file: result.out });
-      });
-    } else {
-      console.log(err);
-    }
-  });
-};
-
-export { readFiles, readFile };
+export { readFile };
